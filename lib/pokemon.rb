d3bb093fd58b702eeaf316078ec5d9a3ef9eecf3
sophie <justanothersophie@gmail.com>
2021-08-16 16:54:56 -0400

    commit tree 4741c1bad5d188aa9d31de9dfa9208efdb5672f4

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
+require_relative '../bin/environment.rb'
+
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+
+    def initialize(name:, type:, id: nil, db:)
+        self.name = name
+        self.type = type
+        self.db = db
+        self.id = id
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ? LIMIT 1"
+        db.execute(sql, id).collect do |row|
+            Pokemon.new(name: row[1], type: row[2], id: row[0], db: db)
+        end.first
+    end
+
 end
